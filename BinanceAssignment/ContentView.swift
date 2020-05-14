009d24e1f7d74be0bb3c42b4168475c2866cbb21
LeiHao <leihao.sg@gmail.com>
2020-05-14 23:40:29 +0800

    Initial Commit

---
+++ BinanceAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BinanceAssignment
+//
+//  Created by LH on 5/14/20.
+//  Copyright © 2020 LH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
